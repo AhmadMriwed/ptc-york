1540a87506becce2bebe3c8a04ca0be06c3adf1a
AhmadMriwed <mr.ahmadmriwed@gmail.com>
2024-06-21 23:05:24 +0300

    fix api route

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::middleware(['set_user_language'])->group(function () {
+Route::group(function () {
     Route::post('register', [UserAuthController::class, 'register']);
     Route::post('login', [UserAuthController::class, 'login']);
 
